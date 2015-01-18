45da00995f4f1ce383559f879ff8164e1851c5ef
Richard Cooke <cookeiii@umich.edu>
2015-01-18 03:01:17 -0500

    small

--- classcompiler/js/scripts.js
+++ classcompiler/js/scripts.js
@@ -84,6 +84,7 @@ function testAPI() {
 		_global_name = response.name;
 		_global_email = response.email;
 		
+		console.log("is startPage defined?", startPage);
 		var startPage = (startPage || function(){ console.log("nothing happened.") })();
 	});
 }
