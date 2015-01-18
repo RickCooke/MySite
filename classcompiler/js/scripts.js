b9aa14688a1e44a60df3e263e22b275938fe51f3
Richard Cooke <cookeiii@umich.edu>
2015-01-17 19:17:20 -0500

    small

--- classcompiler/js/scripts.js
+++ classcompiler/js/scripts.js
@@ -1,7 +1,22 @@
-$(document).ready(function() {
-  
-   $("table tr:nth-child(3)").click(function() {
-       var ret = prompt("Please add a class name: ");
-       alert("You have entered: " +  ret);
-   });
+( function( $ ) {
+$( document ).ready(function() {
+$('#cssmenu li.has-sub>a').on('click', function(){
+		$(this).removeAttr('href');
+		var element = $(this).parent('li');
+		if (element.hasClass('open')) {
+			element.removeClass('open');
+			element.find('li').removeClass('open');
+			element.find('ul').slideUp();
+		}
+		else {
+			element.addClass('open');
+			element.children('ul').slideDown();
+			element.siblings('li').children('ul').slideUp();
+			element.siblings('li').removeClass('open');
+			element.siblings('li').find('li').removeClass('open');
+			element.siblings('li').find('ul').slideUp();
+		}
+	});
+
 });
+} )( jQuery );
